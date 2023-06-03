38bdcdbded46244064d15407773fe5542fc22d58
Sandy Budi Wirawan <sandybudiwirawan@gmail.com>
2023-06-03 15:22:12 +0700

    [add route web & env vite for daisy ui plugin]

--- routes/web.php
+++ routes/web.php
@@ -15,24 +15,43 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('index');
-})->name('user.index');
+Route::get("/", function () {
+    return view("index");
+})->name("user.index");
 
-Route::prefix('auth')->group(function () {
-    Route::prefix('register')->group(function () {
-        Route::get('/', [RegisterController::class, 'show'])->name('register');
-        Route::post('/', [RegisterController::class, 'store'])->name('register');
+Route::prefix("auth")->group(function () {
+    Route::prefix("register")->group(function () {
+        Route::get("/", [RegisterController::class, "show"])->name("register");
+        Route::post("/", [RegisterController::class, "store"])->name(
+            "register"
+        );
     });
 
-    Route::prefix('login')->middleware('guest:admin,user')->group(function () {
-        Route::get('/', [LoginController::class, 'show'])->name('login');
-        Route::post('/', [LoginController::class, 'login'])->name('login');
-    });
+    Route::prefix("login")
+        ->middleware("guest:admin,user")
+        ->group(function () {
+            Route::get("/", [LoginController::class, "show"])->name("login");
+            Route::post("/", [LoginController::class, "login"])->name("login");
+        });
 });
 
-Route::group(['prefix' => 'dashboard', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
-    Route::get('/', function () {
-        return view('admin.index');
-    })->name('index');
+Route::group(
+    ["prefix" => "dashboard", "as" => "admin.", "middleware" => "auth:admin"],
+    function () {
+        Route::get("/", function () {
+            return view("admin.index");
+        })->name("index");
+    }
+);
+
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/login", function () {
+    return view("login");
+});
+
+Route::get("/register", function () {
+    return view("register");
 });
