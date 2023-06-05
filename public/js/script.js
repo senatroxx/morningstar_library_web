53ea7f0594d33a1354deb35265aec94a5405320c
Sandy Budi Wirawan <sandybudiwirawan@gmail.com>
2023-06-05 22:02:17 +0700

    fixing(index): view

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+// Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
+});
+
+// Out hamburger
+window.addEventListener("click", function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove("hamburger-active");
+        navMenu.classList.add("hidden");
+    }
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,114 +35,113 @@
                         <ul class="block lg:flex">
                             <li class="group">
                                 <a class="text-dark mx-8 flex py-2 text-base group-hover:text-primary dark:text-white"
-                                    href="#home">Home</a>
+                                    href="#">Home</a>
                             </li>
                             <li class="group">
                                 <a class="text-dark mx-8 flex py-2 text-base group-hover:text-primary dark:text-white"
-                                    href="#about">Feature</a>
+                                    href="#">Feature</a>
                             </li>
                             <li class="group">
                                 <a class="text-dark mx-8 flex py-2 text-base group-hover:text-primary dark:text-white"
-                                    href="#portfolio">About</a>
+                                    href="#">About</a>
                             </li>
                             <li class="group">
                                 <a class="text-dark mx-8 flex py-2 text-base group-hover:text-primary dark:text-white"
-                                    href="#clients">Team</a>
+                                    href="#">Team</a>
                             </li>
                             <li class="group">
                                 <a class="text-dark mx-8 flex py-2 text-base group-hover:text-primary dark:text-white"
-                                    href="#blog">FAQ</a>
-                            </li>                  
-                         </ul>
-                        
+                                    href="#">FAQ</a>
+                            </li>
+                        </ul>
+
                         <div class="px-4 py-9">
                             @guest('user') @guest('admin')
                             <a class="mb-7 rounded-lg bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 href="{{ route('login') }}">Login</a>
                             @endguest @endguest
-                    
+
                             @auth('user')
                                 <a class="mb-7 rounded-lg bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                     href="#">
                                     {{ Auth::guard('user')->user()->name }}
                                 </a>
                             @endauth
-                    
+
                             @auth('admin')
                                 <a class="mb-7 rounded-lg bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                     href={{ route('admin.index') }}>
                                     Dashboard
                                 </a>
                             @endauth
-        
-                         </div>
+
+                        </div>
                     </nav>
                 </div>
 
+                <div class="nav-container mb-7 hidden justify-center lg:flex lg:gap-x-20">
+                    <a class="text-sm font-semibold leading-6 text-gray-900" href="#">Home</a>
 
-    <div class="nav-container mb-7  hidden justify-center lg:flex lg:gap-x-20">
-        <a class="text-sm font-semibold leading-6 text-gray-900" href="#">Home</a>
+                    <a class="text-sm font-semibold leading-6 text-gray-900" href="#">Features</a>
 
-        <a class="text-sm font-semibold leading-6 text-gray-900" href="#">Features</a>
+                    <a class="text-sm font-semibold leading-6 text-gray-900" href="#">About</a>
 
-        <a class="text-sm font-semibold leading-6 text-gray-900" href="#">About</a>
+                    <a class="text-sm font-semibold leading-6 text-gray-900" href="#">Team</a>
+                    <a class="text-sm font-semibold leading-6 text-gray-900" href="#">FAQ</a>
+                </div>
+                <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+                    @guest('user') @guest('admin')
+                    <a class="mb-7 rounded-lg bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        href="{{ route('login') }}">Login</a>
+                    @endguest @endguest
 
-        <a class="text-sm font-semibold leading-6 text-gray-900" href="#">Team</a>
-        <a class="text-sm font-semibold leading-6 text-gray-900" href="#">FAQ</a>
-    </div>
-    <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-        @guest('user') @guest('admin')
-        <a class="mb-7 rounded-lg bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            href="{{ route('login') }}">Login</a>
-        @endguest @endguest
+                    @auth('user')
+                        <a class="mb-7 rounded-lg bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            href="#">
+                            {{ Auth::guard('user')->user()->name }}
+                        </a>
+                    @endauth
 
-        @auth('user')
-            <a class="mb-7 rounded-lg bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                href="#">
-                {{ Auth::guard('user')->user()->name }}
-            </a>
-        @endauth
+                    @auth('admin')
+                        <a class="mb-7 rounded-lg bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            href={{ route('admin.index') }}>
+                            Dashboard
+                        </a>
+                    @endauth
 
-        @auth('admin')
-            <a class="mb-7 rounded-lg bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                href={{ route('admin.index') }}>
-                Dashboard
-            </a>
-        @endauth
+                </div>
+            </nav>
+        </header>
 
-     </div>
-    </nav>
-</header>
+        <div class="relative isolate px-6 pt-14 lg:px-8">
+            <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                aria-hidden="true">
+                <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                    style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+                </div>
+            </div>
 
-    <div class="relative isolate px-6 pt-14 lg:px-8">
-        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            <div class="x-auto py-32 text-center sm:py-48 lg:py-56">
+                <img class="mx-auto mb-7" src="https://i.postimg.cc/15BQDj4w/logo.png" alt="">
+                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Open - Source Library
+                    Application</h1>
+                <p class="mt-6 text-lg leading-8 text-gray-600">Made with Tailwind and Laravel.
+                    Supported Multi - Platform</p>
+                <div class="mt-10 flex items-center justify-center gap-x-6">
+                    <a class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        href="{{ route('register') }}">Sign Up!</a>
+                    <a class="text-sm font-semibold leading-6 text-gray-900"
+                        href="https://github.com/senatroxx/morningstar_library_web">Star on Github <span
+                            aria-hidden="true">→</span></a>
+                </div>
+            </div>
+        </div>
+        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
             aria-hidden="true">
-            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
                 style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
             </div>
         </div>
-
-        <div class="x-auto py-32 text-center sm:py-48 lg:py-56">
-            <img class="mx-auto mb-7" src="https://i.postimg.cc/15BQDj4w/logo.png" alt="">
-            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Open - Source Library
-                Application</h1>
-            <p class="mt-6 text-lg leading-8 text-gray-600">Made with Tailwind and Laravel.
-                Supported Multi - Platform</p>
-            <div class="mt-10 flex items-center justify-center gap-x-6">
-                <a class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    href="{{ route('register') }}">Sign Up!</a>
-                <a class="text-sm font-semibold leading-6 text-gray-900"
-                    href="https://github.com/senatroxx/morningstar_library_web">Star on Github <span
-                        aria-hidden="true">→</span></a>
-            </div>
-        </div>
-    </div>
-    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        aria-hidden="true">
-        <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
-        </div>
-    </div>
     </div>
     </div>
     {{--  End Hero Section  --}}
